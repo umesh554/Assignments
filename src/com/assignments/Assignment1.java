56088633e8296ab073c4f4bd642ea5120e8a0fd1
Umesh Kadam <umeshkadam7066@gmail.com>
2025-03-12 08:50:37 +0530

    Assignment 1 - Data Types

--- src/com/assignments/Assignment1.java
+++ src/com/assignments/Assignment1.java
@@ -3,7 +3,122 @@ package com.assignments;
 public class Assignment1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+	float temperature = 25.5f;
+	System.out.println(temperature);
+	
+	//
+	
+	boolean Order = true;
+	boolean Order2 = false;
+	
+	System.out.println(Order);
+	System.out.println(Order2);
+	
+	//
+	
+	
+	String number = "123-456-7890";
+	System.out.println(number);
+	
+	//
+	
+	double Money = 1000.50;
+	System.out.println(Money);
+	
+	//
+	
+	String Email = "John.doe@example.com";
+	System.out.println(Email);
+	
+	//
+	
+	double latitude = 37.7749; 
+	double longitude = -122.4194;
+	        
+	System.out.println("latitude:" +latitude);
+	System.out.println("longitude:" +longitude);
+	
+	//
+	
+	boolean status = true;
+	boolean status2 = false;
+	
+	System.out.println(status);
+	System.out.println(status2);
+	
+	//
+	
+	String occupation = "Software Engineer";
+	System.out.println(occupation);
+	
+	//
+	
+	String colour = "Blue";
+	System.out.println(colour);
+	
+	//
+	
+	short year = 2023;
+	System.out.println(year);
+	
+	//
+	
+	String followers = "1,000,000";
+	System.out.println(followers);
+	
+	//
+	
+	float Rating = 7.5f;
+	System.out.println(Rating);
+	
+	//
+	
+	char bloodtype = 'A';
+	System.out.println(bloodtype);
+	
+	//
+	
+	String TitleOfBook = "To Kill a Mockingbird";
+	System.out.println(TitleOfBook);
+	
+	//
+	
+	short employees = 500;
+	System.out.println(employees);
+	
+	//
+	
+	String TimeOfAnEvent = "2:30 PM";
+	System.out.println(TimeOfAnEvent);
+	
+	//
+	
+	String Country = "United States";
+	System.out.println(Country);
+	
+	//
+	
+	String EyeColour = "Brown";
+	System.out.println(EyeColour);
+	
+	//
+	
+	String Birthplace = "New York City";
+	System.out.println(Birthplace);
+	
+	//
+	
+	float Distance = 200.5f;
+	System.out.println(Distance);
+	
+	//
+	
+	
+	
+	
+		
+	
 
 	}
 
